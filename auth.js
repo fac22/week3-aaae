360b1fd02eaaaf652862349d41e0fa3139e68106
aaadriana <adriana@stanciu.uk>
2021-10-13 16:18:00 +0100

    adeded verifyUser fct in auth.js
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -4,10 +4,31 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const model = require('./database/model');
 
+function verifyUser(email, password) {
+  model
+    .getUser(email)
+    .then((user) => bcrypt.compare(password, user.password))
+    .then((match) => {
+      if (!match) {
+        throw new Error('Password mismatch');
+      } else {
+        delete user.password;
+        return user;
+      }
+    });
+}
+
 function createUser(email, password, name) {
   return bcrypt
     .hash(password, 10)
     .then((hash) => model.createUser(email, hash, name));
 }
 
-module.exports = { createUser };
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  maxAge: 600000,
+  sameSite: 'strict',
+  signed: true,
+};
+
+module.exports = { verifyUser, createUser, COOKIE_OPTIONS };
