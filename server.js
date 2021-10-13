4078efb43fa8f5fb3effa502ed372a55233d1b74
Elena <rhapsodyenmauve@gmail.com>
2021-10-13 14:42:20 +0100

    setting login routes in the server.js
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const express = require('express');
+const login = require('./routes/login');
 
 const server = express();
+
+const bodyParser = express.urlencoded({ extended: false });
 
 server.get('/', (request, response) => {
   response.send('<h1>Hello</h1>');
 });
+// setting the routes for login - get and set
+// implementation of the functions is in the ./routes/login.js
+server.get('/login', login.get);
+server.post('/login', login.post);
 
 const PORT = process.env.PORT || 3000;
 
