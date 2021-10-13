9e4d6743c56fe75847154076e153e51161af6601
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-13 17:08:08 +0100

    login is now log-in
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>

--- server.js
+++ server.js
@@ -8,13 +8,9 @@ const db = require('./database/connection.js');
 
 const server = express();
 
-
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
-
-
-
 
 server.use(express.urlencoded({ extended: false }));
 
@@ -27,15 +23,13 @@ server.get('/', (request, response) => {
   response.send('<h1>Hello</h1>');
 });
 
-
 // setting the routes for login - get and set
 // implementation of the functions is in the ./routes/login.js
-server.get('/login', login.get);
-server.post('/login', login.post);
+server.get('/log-in', login.get);
+server.post('/log-in', login.post);
 
 server.get('/sign-up', signUp.get);
 server.post('/sign-up', signUp.post);
-
 
 const PORT = process.env.PORT || 3000;
 
