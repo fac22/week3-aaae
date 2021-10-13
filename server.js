540cd3a1cbd08543388683e0684ad79020efd47a
“Moggach” <acunnane13@gmail.com>
2021-10-13 14:25:00 +0100

    setting up home route on server
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const server = express();
+
+server.get('/', (request, response) => {
+  response.send('<h1>Hello</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
