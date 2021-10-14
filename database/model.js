fb7b7db54d26712458eac21fa8c0e034ad7b4cb3
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-14 16:02:15 +0100

    deleted sql syntax highlighting comment
    Co-authored-by: Adriana <82960475+aaadriana@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -4,7 +4,7 @@ const db = require('./connection.js');
 
 function createUser(email, hash, name) {
   console.log('creating user with ', email, hash, name);
-  const INSERT_USER = /*sql*/ `
+  const INSERT_USER = `
       INSERT INTO users (email, password, name) VALUES ($1, $2, $3)
       RETURNING id, email, name
     `;
@@ -17,14 +17,14 @@ function createUser(email, hash, name) {
 }
 
 function getUser(email) {
-  const SELECT_USER = /*sql*/ `
+  const SELECT_USER = `
     SELECT id, email, password, name FROM users WHERE email=$1
     `;
   return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
 }
 
 function createSession(sid, data) {
-  const INSERT_SESSION = /*sql*/ `
+  const INSERT_SESSION = `
   INSERT INTO sessions (sid, data) VALUES ($1, $2)
   RETURNING sid`;
   return db
@@ -33,7 +33,7 @@ function createSession(sid, data) {
 }
 
 function getSession(sid) {
-  const SELECT_SESSION = /*sql*/ `SELECT data FROM sessions WHERE sid=$1`;
+  const SELECT_SESSION = `SELECT data FROM sessions WHERE sid=$1`;
   return db.query(SELECT_SESSION, [sid]).then((result) => {
     const singleResult = result.rows[0];
     return singleResult && singleResult.data;
@@ -41,24 +41,24 @@ function getSession(sid) {
 }
 
 function createPost(id, textContent) {
-  const INSERT_POST = /*sql*/ `INSERT INTO posts (user_id, text_content, created_at) VALUES ($1, $2, (SELECT CURRENT_TIMESTAMP))`;
+  const INSERT_POST = `INSERT INTO posts (user_id, text_content, created_at) VALUES ($1, $2, (SELECT CURRENT_TIMESTAMP))`;
   return db.query(INSERT_POST, [id, textContent]);
 }
 
 function getPosts() {
-  const SELECT_POST = /*sql*/ `
+  const SELECT_POST = `
     SELECT id, text_content, user_id FROM posts
     `;
   return db.query(SELECT_POST).then((result) => result.rows);
 }
 
 function deletePost(postId, user_id) {
-  const DELETE_POST = /*sql*/ `DELETE FROM posts WHERE id=$1 AND user_id=$2`;
+  const DELETE_POST = `DELETE FROM posts WHERE id=$1 AND user_id=$2`;
   return db.query(DELETE_POST, [parseInt(postId, 10), user_id]);
 }
 
 function deleteSession(sid) {
-  const DELETE_SESSION = /*sql*/ `DELETE FROM sessions WHERE sid=$1`;
+  const DELETE_SESSION = `DELETE FROM sessions WHERE sid=$1`;
   return db.query(DELETE_SESSION, [sid]);
 }
 
