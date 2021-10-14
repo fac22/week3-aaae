14ec19b40da4aa6c23679d22620ee7d47ecd8e3c
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-14 16:38:44 +0100

    added ssl option to connection.js from https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js

--- database/connection.js
+++ database/connection.js
@@ -5,14 +5,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const DB_URL = process.env.DATABASE_URL;
+// const DB_URL = process.env.DATABASE_URL;
 
-if (!DB_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error('Please set the DATABASE_URL environment variable');
 }
 
 const options = {
-  connectionString: DB_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const db = new pg.Pool(options);
